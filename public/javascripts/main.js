bbcd04368a0066c9568764d891aa69bb242c946a
cold_hailstone <cksn1993@gmail.com>
2021-05-02 16:58:24 +0900

    여러개의 마커 표시하기

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,3 +4,27 @@ const mapOptions = {
 };
 
 const map = new naver.maps.Map('map', mapOptions);
+
+const data = [
+    {
+        title: "용산역",
+        address: "용산",
+        lat: 37.530107579847304,
+        lng: 126.96479313805436
+    },
+    {
+        title: "서울역",
+        address: "서울역",
+        lat: 37.55324891278443,
+        lng: 126.97260760696861
+    }
+];
+
+for (const i in data) {
+    const target = data[i];
+    const latlng = new naver.maps.LatLng(target.lat, target.lng);
+    const marker = new naver.maps.Marker({
+        map,
+        position: latlng
+    });
+}
