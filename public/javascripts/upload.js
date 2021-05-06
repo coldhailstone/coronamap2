3915270501b1c8e962154072339a3513e60d323c
cold_hailstone <cksn1993@gmail.com>
2021-05-06 14:59:15 +0900

    키워드 검색 데이터 받아오기

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -5,3 +5,28 @@ const mapOption = {
 };
 
 let map = new daum.maps.Map(mapContainer, mapOption);
+
+let infowindow = new daum.maps.InfoWindow({
+    zIndex: 1
+});
+
+let markerList = [];
+
+const ps = new daum.maps.services.Places();
+searchPlaces();
+
+function searchPlaces() {
+    const keyword = $('#keyword').val();
+    ps.keywordSearch(keyword, placesSearchCB);
+    
+}
+
+function placesSearchCB(data, status) {
+    if (status === daum.maps.services.Status.OK) {
+        console.log(data);
+    } else if (status === daum.maps.services.Status.ZERO_RESULT) {
+        alert('검색 결과가 존재하지 않습니다.');
+    } else if (status === daum.maps.services.Status.ERROR) {
+        alert('검색 결과 중 오류가 발생했습니다.');
+    }
+}
