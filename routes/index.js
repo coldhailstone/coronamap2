e18495794debf7d1568aaf3dd2c7481ed23d7488
cold_hailstone <cksn1993@gmail.com>
2021-05-10 14:11:32 +0900

    Postman 테스트 해보기

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 var express = require('express');
 var router = express.Router();
 
@@ -8,6 +9,22 @@ router.get('/', (req, res, next) => {
 
 router.get('/upload', (req, res, next) => {
   res.render('upload');
+});
+
+router.get('/test', (req, res, next) => {
+  console.log("테스트 완료!");
+  res.json({
+    message: 'reponse 완료!'
+  });
+});
+
+router.post('/test2', (req, res, next) => {
+  const {test, test2} = req.body;
+  console.log(test, test2);
+  
+  res.json({
+    message: 'post 완료!'
+  });
 });
 
 module.exports = router;
