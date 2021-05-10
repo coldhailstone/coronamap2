2087f03d6169608d1fa7820722c9749e3a6a3165
cold_hailstone <cksn1993@gmail.com>
2021-05-10 14:34:44 +0900

    지도 위에 저장한 데이터 표시하기

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,63 +5,56 @@ const mapOptions = {
 
 const map = new naver.maps.Map('map', mapOptions);
 
-const data = [
-    {
-        title: '용산역',
-        address: '용산',
-        lat: 37.530107579847304,
-        lng: 126.96479313805436
-    },
-    {
-        title: '서울역',
-        address: '서울역',
-        lat: 37.55324891278443,
-        lng: 126.97260760696861
+$.ajax({
+    url: '/location',
+    type: 'GET'
+}).done(res => {
+    if (res.message !== 'success') return;
+    const data = res.data;
+
+    let markerList = [];
+    let infowindowList = [];
+
+    const getClickHandler = i => () => {
+        const infowindow = infowindowList[i];
+        infowindow.getMap() ? infowindow.close() : infowindow.open(map, markerList[i]);
+    };
+
+    const getClickMap = i => () => {
+        infowindowList[i].close();
+    };
+
+    for (const i in data) {
+        const target = data[i];
+        const latlng = new naver.maps.LatLng(target.lat, target.lng);
+        const marker = new naver.maps.Marker({
+            map,
+            position: latlng,
+            icon: {
+                content: `<div class='marker'></div>`,
+                anchor: new naver.maps.Point(7.5, 7.5)
+            }
+        });
+
+        const content = `
+        <div class='infowindow_wrap'>
+            <div class='infowindow_title'>${target.title}</div>
+            <div class='infowindow_address'>${target.address}</div>
+        </div>`;
+
+        const infowindow = new naver.maps.InfoWindow({
+            content,
+            backgroundColor: '#00ff0000',
+            borderColor: '#00ff0000',
+            anchorSize: new naver.maps.Size(0, 0)
+        });
+
+        markerList.push(marker);
+        infowindowList.push(infowindow);
     }
-];
 
-let markerList = [];
-let infowindowList = [];
-
-const getClickHandler = i => () => {
-    const infowindow = infowindowList[i];
-    infowindow.getMap() ? infowindow.close() : infowindow.open(map, markerList[i]);
-};
-
-const getClickMap = i => () => {
-    infowindowList[i].close();
-};
-
-for (const i in data) {
-    const target = data[i];
-    const latlng = new naver.maps.LatLng(target.lat, target.lng);
-    const marker = new naver.maps.Marker({
-        map,
-        position: latlng,
-        icon: {
-            content: `<div class='marker'></div>`,
-            anchor: new naver.maps.Point(7.5, 7.5)
-        }
-    });
-
-    const content = `
-    <div class='infowindow_wrap'>
-        <div class='infowindow_title'>${target.title}</div>
-        <div class='infowindow_address'>${target.address}</div>
-    </div>`;
-
-    const infowindow = new naver.maps.InfoWindow({
-        content,
-        backgroundColor: '#00ff0000',
-        borderColor: '#00ff0000',
-        anchorSize: new naver.maps.Size(0, 0)
-    });
-
-    markerList.push(marker);
-    infowindowList.push(infowindow);
-}
-
-for (let i = 0, ii = markerList.length; i < ii; i++) {
-    naver.maps.Event.addListener(markerList[i], 'click', getClickHandler(i));
-    naver.maps.Event.addListener(map, 'click', getClickMap(i));
-}
+    for (let i = 0, ii = markerList.length; i < ii; i++) {
+        naver.maps.Event.addListener(markerList[i], 'click', getClickHandler(i));
+        naver.maps.Event.addListener(map, 'click', getClickMap(i));
+    }
+});
