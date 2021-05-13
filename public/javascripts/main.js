127dc1cb663949aaa14054939268ecf948d7c091
cold_hailstone <cksn1993@gmail.com>
2021-05-13 11:58:41 +0900

    행정구역 데이터 레이어 소개 및 표시하기

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -61,7 +61,7 @@ $.ajax({
     const cluster1 = { content: `<div class='cluster1'></div>` };
     const cluster2 = { content: `<div class='cluster2'></div>` };
     const cluster3 = { content: `<div class='cluster3'></div>` };
-    const MarkerClustering = new MarkerClustering({
+    const markerClustering = new MarkerClustering({
         minClusterSize: 2,
         maxZoom: 12,
         map,
@@ -69,9 +69,63 @@ $.ajax({
         disableClickZoom: false,
         gridSize: 20,
         icons: [cluster1, cluster2, cluster3],
-        indexGenerator: [2, 5, 10],
+        indexGernerator: [2, 5, 10],
         stylingFunction: (clusterMarker, count) => {
             $(clusterMarker.getElement()).find('div:first-child').text(count);
         }
     });
 });
+
+const urlPrefix = "https://navermaps.github.io/maps.js/docs/data/region";
+const urlSuffix = ".json";
+
+let regionGeoJson = [];
+let loadCount = 0;
+
+const tooltip = $(
+    `<div style='position: absolute; z-index: 1000; padding: 5px 10px; background: white; border: 1px sold black; font-size: 14px; display: none; pointer-events: none;'></div>`
+);
+tooltip.appendTo(map.getPanes().floatPane);
+
+naver.maps.Event.once(map, 'init_stylemap', () => {
+    for (let i = 1; i < 1; i++) {
+        let keyword = i.toString();
+        if (keyword.length === 1)
+            keyword = "0" + keyword;
+
+        $.ajax({
+            url: urlPrefix + keyword + urlSuffix
+        }).done(geojson => {
+            regionGeoJson.push(geojson);
+            loadCount++;
+            if (loadCount === 17)
+                startDataLayer();
+        });
+    }
+});
+
+function startDataLayer() {
+    map.data.setStyle(feature => {
+        const styleOptions = {
+            fillColor: '#ff0000',
+            fillOpacity: 0.0001,
+            strokeColor: '#ff0000',
+            strokeWeight: 2,
+            strokeOpacity: 0.4
+        };
+
+        if (feature.getProperty('focus')) {
+            styleOptions.fillColor = '#0f0';
+            styleOptions.fillOpacity = 0.6;
+            styleOptions.strokeColor = '#0f0';
+            styleOptions.strokeWeight = 4;
+            styleOptions.strokeOpacity = 1;
+        }
+
+        return styleOptions;
+    });
+
+    regionGeoJson.forEach(geojson => {
+        map.data.addGeoJson(geojson);
+    });
+}
